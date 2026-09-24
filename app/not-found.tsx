85af58920daaf340979d662378e4b2731c121cc4
Abdullah Al Sabbir <abdullahals128@gmail.com>
2026-09-25 00:47:29 +0600

    adding not found

---
+++ app/not-found.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function NotFound(){return <main className="not-found"><p className="eyebrow">404 / LOST REP</p><h1>THIS PAGE ISN&apos;T IN THE LOG.</h1><p>Looks like this route skipped leg day.</p><Link className="button primary" href="/">Back to workouts</Link></main>}
